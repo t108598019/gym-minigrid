e304f149bb5024939dbcfe186c2243bf81897d99
108598019 <t108598019@ntut.org.tw>
2021-03-17 18:21:05 +0800

    add empty 16x16

---
+++ run_empty_16x16.py
@@ -0,0 +1,20 @@
+import os
+import matplotlib.pyplot as plt
+
+from gym_minigrid.wrappers import *
+from gym_minigrid.wrappers import FlatObsWrapper
+from stable_baselines import PPO2
+from stable_baselines.common.vec_env import  DummyVecEnv
+
+tensorboard_folder = './tensorboard/MiniGrid-Empty-16x16/'
+model_folder = './models/MiniGrid-Empty-16x16/'
+if not os.path.isdir(tensorboard_folder):
+    os.makedirs(tensorboard_folder)
+if not os.path.isdir(model_folder):
+    os.makedirs(model_folder)
+
+env = gym.make('MiniGrid-Empty-16x16-v0')
+env = FlatObsWrapper(env)
+model = PPO2('MlpPolicy', env, verbose=0, nminibatches=1, n_steps=128,tensorboard_log=tensorboard_folder)
+model.learn(total_timesteps=1000000, tb_log_name='PPO2')
+model.save(model_folder + "PPO2")
