2054e8ab3efa8feba52f08e4128f12928de95fcc
108598019 <t108598019@ntut.org.tw>
2021-03-22 15:57:14 +0800

    fix not tty

--- scripts/run_docker_gpu.sh
+++ scripts/run_docker_gpu.sh
@@ -7,7 +7,7 @@ cmd_line="$@"
 echo "Executing in the docker (gpu image):"
 echo $cmd_line
 
-docker run --gpus all -it --rm --network host --ipc=host \
+docker run --gpus all -i --rm --network host --ipc=host \
  --mount src=${SB_PATH}/stable_baselines/,target=/root/code/stable_baselines/,type=bind \
  --mount src=$(pwd),target=/root/code/env/,type=bind \
   ntutselab/stable-baselines-gpu:latest \
