cfa0bc63d5106d261eaeaf4c6574bd15c97b0114
108598019 <t108598019@ntut.org.tw>
2021-03-22 23:34:18 +0800

    TRAIN FOUR ROOMS 1M STEPS 0.0025

--- run_four_rooms.py
+++ run_four_rooms.py
@@ -6,7 +6,7 @@ from gym_minigrid.wrappers import FlatObsWrapper
 from stable_baselines import PPO2
 from stable_baselines.common.vec_env import  DummyVecEnv
 
-tensorboard_folder = './tensorboard/MiniGrid-FourRooms/'
+tensorboard_folder = '/root/code/stable_baselines/tensorboard/MiniGrid-FourRooms/'
 model_folder = './models/MiniGrid-FourRooms/'
 if not os.path.isdir(tensorboard_folder):
     os.makedirs(tensorboard_folder)
